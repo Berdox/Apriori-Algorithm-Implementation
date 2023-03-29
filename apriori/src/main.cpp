1cd2b64ee47d1161f352670111bbfdfcaa82afdd
dustin-ward <wardddustin@gmail.com>
2023-03-29 15:15:54 -0600

    added filename arg

--- apriori/src/main.cpp
+++ apriori/src/main.cpp
@@ -1,10 +1,16 @@
-
-
+#include <iostream>
+#include <string>
 #include "../include/Apriori.h"
 
-// need to add arguments to the exe
-int main() {
+int main(int argc, char* argv[]) {
+    if(argc < 2) {
+        std::cerr << "no filename provided" << std::endl;
+        return -1;
+    }
+
+    std::string filename = argv[1];
+
     Apriori a;
-    a.aprioriRun("./databases/testData.txt", 0.22);
+    a.aprioriRun(filename, 0.22);
     return 0;
 }
