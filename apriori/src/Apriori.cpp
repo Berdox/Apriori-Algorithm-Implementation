454c69265942ff5f099c500ff7c22471f66d3652
dustin-ward <wardddustin@gmail.com>
2023-03-29 19:00:37 -0600

    K=1 itemsets

--- apriori/src/Apriori.cpp
+++ apriori/src/Apriori.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <algorithm>
 #include <ostream>
+#include <cmath>
 
 Apriori::Apriori(std::string dbName, double minSup) : minSup(minSup) {
     // Perform initial scan of file
@@ -15,7 +16,7 @@ Apriori::Apriori(std::string dbName, double minSup) : minSup(minSup) {
     }
 
     // Threshold for number of occurances to call frequent
-    minSupCount = (int)(minSup * float(ret)); 
+    minSupCount = (int)std::ceil(minSup * float(ret)); 
 }
 
 int Apriori::readDataBase(std::string dbName) {
@@ -50,95 +51,23 @@ int Apriori::readDataBase(std::string dbName) {
     return transactions.size();
 }
 
-// std::map<std::string, int> Apriori::scanDataBase(std::map<std::string, int> collection) {
-//     std::ifstream file;
-//     file.open(dataBaseName);
-//     if(!file.is_open()) {
-//         std::cout << "not open" << std::endl;
-//         return collection;
-//     }
-//
-//     // read the database
-//     std::string line;
-//     while (getline (file, line)) {
-//         std::map<std::string, int> pairs;
-//
-//         line += ",";
-//
-//         std::istringstream iss(line);
-//         std::string item;
-//         while(iss>>item) {
-//             item = item.substr(0, item.length()-1);
-//             pairs[item]++;
-//         }
-//
-//         for(auto it = collection.begin(); it != collection.end(); it++) {
-//             std::vector<std::string> colFreq;
-//             std::string freq = it->first;
-//             std::cout << "freq: " << freq << std::endl;
-//             for(unsigned int i = 1; i < freq.size(); i++) {
-//                 if(freq[i] == '\n' || freq[i] == 'i') {
-//                     //std::cout << "sub " << freq.substr(0, i) << '\n';
-//                     colFreq.push_back(freq.substr(0, i));
-//                     freq.erase(0, i);
-//                     i = 1;
-//                 }
-//                 std::cout << "freq2: " << freq << std::endl;
-//             }
-//             for(auto p : colFreq) {
-//                 std::cout << "col " << p << '\n';
-//             }
-//             std::cout << "break \n";
-//             /*unsigned int count = 0;
-//             for(unsigned int i = 0; i < colFreq.size(); i++) {
-//                 if(pairs.find(colFreq[i]) != pairs.end()) {
-//                     count++;
-//                 }
-//             }*/
-//
-//             /*if(count == colFreq.size()) {
-//                 it->second++;
-//             }*/
-//         }
-//     }
-//
-//     file.close();
-//
-//     return collection;
-// }
-//
-// std::map<std::string, int> Apriori::prune(std::map<std::string, int> collection, float minSup) {
-//     
-//     std::vector<std::string> keys;
-//
-//     for(auto tran : collection) {
-//         if(float(tran.second) < minSup) {
-//             keys.push_back(tran.first);
-//         }
-//     }
-//
-//     for(auto key : keys) {
-//         collection.erase(key);
-//     }
-//
-//     return collection;
-// }
-//
-// std::map<std::string, int> Apriori::joinItemSets(std::map<std::string, int> collection) {
-//     std::map<std::string, int> join;
-//     std::cout << "joinning " << std::endl;
-//     for(auto it = collection.begin(); it != collection.end(); it++) {
-//         auto temp = it;
-//         temp++;
-//         for(auto itt = temp; itt != collection.end(); itt++) {
-//             join.insert(std::make_pair((it->first + itt->first), 0));
-//         }
-//         
-//     }
-//
-//     return join;
-// }
-//
 std::vector<itemset> Apriori::aprioriRun() {
-    return transactions;
+    // Assuming no duplicate items can exist in any given transaction, gather
+    // the frequencies to compare to minSup
+    std::map<itemset,int> candidate;
+    for(auto &transaction : transactions) {
+        for(auto &item : transaction) {
+            itemset newItemset = {item};
+            candidate[newItemset]++;
+        }
+    }
+
+    // Scan candidates for frequent itemsets
+    std::vector<itemset> frequent;
+    for(auto &[candidateSet, freq] : candidate) {
+        if(freq >= minSupCount)
+            frequent.push_back(candidateSet);
+    }
+
+    return frequent;
 }
--- apriori/src/main.cpp
+++ apriori/src/main.cpp
@@ -8,14 +8,21 @@
  */
 
 int main(int argc, char* argv[]) {
+    // Get filename
     if(argc < 2) {
         std::cerr << "no filename provided" << std::endl;
         return -1;
     }
-
     std::string filename = argv[1];
 
-    Apriori a(filename, 0.22);
+    // Get filename
+    if(argc < 3) {
+        std::cerr << "no min_sup provided" << std::endl;
+        return -1;
+    }
+    double min_sup = std::stod(argv[2]);
+    
+    Apriori a(filename, min_sup);
     auto transactions = a.aprioriRun();
 
     std::cout << "TRANSACTIONS:" << std::endl;
